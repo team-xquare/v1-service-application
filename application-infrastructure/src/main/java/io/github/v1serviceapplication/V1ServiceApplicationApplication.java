b2655202bc2661df5f8507a4c3d6d890a7c9d880
parksangwoo <b01056721617@gmail.com>
2022-05-11 23:41:15 +0900

    ⚡️ :: (#6) hexagonal architecture로 변경

---
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/V1ServiceApplicationApplication.java
@@ -0,0 +1,11 @@
+package io.github.v1serviceapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V1ServiceApplicationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(V1ServiceApplicationApplication.class, args);
+    }
+}
