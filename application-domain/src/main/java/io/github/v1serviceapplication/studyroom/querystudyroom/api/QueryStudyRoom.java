a2231d4c897a4bd0667cb213b1a27d3470bef85d
leeseojune53 <sung07288346@gmail.com>
2022-06-01 01:19:30 +0900

    📑 :: QueryStudyRoom

---
+++ application-domain/src/main/java/io/github/v1serviceapplication/studyroom/querystudyroom/api/QueryStudyRoom.java
@@ -0,0 +1,9 @@
+package io.github.v1serviceapplication.studyroom.querystudyroom.api;
+
+import io.github.v1serviceapplication.studyroom.querystudyroom.api.dto.response.StudyRoomElement;
+
+import java.util.List;
+
+public interface QueryStudyRoom {
+    List<StudyRoomElement> queryStudyRoom();
+}
