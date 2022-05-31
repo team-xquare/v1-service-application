989d9aef69577017142e5ca690b6d6900a1f0da8
leeseojune53 <sung07288346@gmail.com>
2022-06-01 01:19:49 +0900

    ⚡️ :: StudyRoom constructor

--- application-domain/src/main/java/io/github/v1serviceapplication/studyroom/StudyRoom.java
+++ application-domain/src/main/java/io/github/v1serviceapplication/studyroom/StudyRoom.java
@@ -9,6 +9,12 @@ import java.util.UUID;
 @Getter
 @Builder
 public class StudyRoom {
+
+    public StudyRoom(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     private UUID id;
     private String name;
 }
