0d04324c91c0d00ceb40e3e1acd8c62afcb6cdba
parksangwoo <b01056721617@gmail.com>
2022-05-31 22:39:46 +0900

    ♻️ :: (#33) domain

---
+++ application-domain/src/main/java/io/github/v1serviceapplication/picnicapply/Picnic.java
@@ -0,0 +1,20 @@
+package io.github.v1serviceapplication.picnicapply;
+
+import io.github.v1serviceapplication.annotation.Aggregate;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Aggregate
+@Getter
+@Builder
+public class Picnic {
+    private UUID userId;
+    private LocalDate date;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private String reason;
+    private String arrangement;
+}
