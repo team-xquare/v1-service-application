7f80105aeeea53f3cd5bfd67af7ce04efb0edcc1
leeseojune53 <sung07288346@gmail.com>
2022-06-02 12:08:15 +0900

    🐛 :: BaseUUID generate value

--- application-infrastructure/src/main/java/io/github/v1serviceapplication/global/entity/BaseUUIDEntity.java
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/global/entity/BaseUUIDEntity.java
@@ -5,10 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.util.UUID;
@@ -20,8 +18,6 @@ import java.util.UUID;
 @MappedSuperclass
 public abstract class BaseUUIDEntity {
     @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Column(columnDefinition = "BINARY(16)")
     private UUID userId;
 }
