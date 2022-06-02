77763232ebf9386af0a022ddf14c4f75739e3fe7
leeseojune53 <sung07288346@gmail.com>
2022-06-02 12:09:43 +0900

    ⚡️ :: POST /study-room

--- application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/studyroom/presentation/StudyController.java
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/studyroom/presentation/StudyController.java
@@ -1,20 +1,35 @@
 package io.github.v1serviceapplication.domain.studyroom.presentation;
 
+import io.github.v1serviceapplication.domain.studyroom.presentation.dto.request.PostStudyRoomRequest;
 import io.github.v1serviceapplication.domain.studyroom.presentation.dto.response.StudyRoomList;
+import io.github.v1serviceapplication.studyroom.poststudyroom.api.PostStudyRoom;
 import io.github.v1serviceapplication.studyroom.querystudyroom.api.QueryStudyRoom;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RestController("/study-room")
+@RequestMapping("/study-room")
+@RestController
 public class StudyController {
 
     private final QueryStudyRoom queryStudyRoom;
+    private final PostStudyRoom postStudyRoom;
 
     @GetMapping
     public StudyRoomList queryStudyRoomList() {
         return new StudyRoomList(queryStudyRoom.queryStudyRooms());
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PostMapping
+    public void postStudyRoom(@RequestBody PostStudyRoomRequest request) {
+        postStudyRoom.postStudyRoom(request.getStudyRoomId());
     }
 
 }
