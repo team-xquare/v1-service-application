d80d92e3e0161be639ea41bd7a674320084c663b
leeseojune53 <sung07288346@gmail.com>
2022-06-06 02:20:27 +0900

    ⚡️ :: ConfigurationPropertiesScan enable

--- application-infrastructure/src/main/java/io/github/v1serviceapplication/V1ServiceApplicationApplication.java
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/V1ServiceApplicationApplication.java
@@ -2,8 +2,10 @@ package io.github.v1serviceapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class V1ServiceApplicationApplication {
     public static void main(String[] args) {
         SpringApplication.run(V1ServiceApplicationApplication.class, args);
