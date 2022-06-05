04921647c5617252177332ff374f3bbe82849391
leeseojune53 <sung07288346@gmail.com>
2022-06-06 02:24:29 +0900

    📑 :: JwtInvalidSignatureException

---
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/global/error/exception/JwtInvalidSignatureException.java
@@ -0,0 +1,12 @@
+package io.github.v1serviceapplication.global.error.exception;
+
+import io.github.v1serviceapplication.error.ApplicationException;
+import io.github.v1serviceapplication.error.ErrorCode;
+
+public class JwtInvalidSignatureException extends ApplicationException {
+    public static final ApplicationException EXCEPTION = new JwtInvalidSignatureException();
+
+    private JwtInvalidSignatureException() {
+        super(ErrorCode.JWT_SIGNATURE);
+    }
+}
