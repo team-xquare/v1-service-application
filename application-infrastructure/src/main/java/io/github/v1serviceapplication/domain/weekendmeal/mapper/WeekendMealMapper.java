f6ccb5b7eea566d55fff6340a32d6b758b037c44
leeseojune53 <sung07288346@gmail.com>
2022-06-09 23:52:16 +0900

    📑 :: WeekendMealMapper

---
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/weekendmeal/mapper/WeekendMealMapper.java
@@ -0,0 +1,12 @@
+package io.github.v1serviceapplication.domain.weekendmeal.mapper;
+
+import io.github.v1serviceapplication.domain.weekendmeal.domain.WeekendMealEntity;
+import io.github.v1serviceapplication.weekendmeal.WeekendMeal;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface WeekendMealMapper {
+    WeekendMealEntity domainToEntity(WeekendMeal weekendMeal);
+
+    WeekendMeal entityToDomain(WeekendMealEntity weekendMeal);
+}
