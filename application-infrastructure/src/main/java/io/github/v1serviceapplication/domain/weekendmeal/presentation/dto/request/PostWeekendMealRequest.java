8334df75d7d6816e1ad88b945315bcc16ac0406c
leeseojune53 <sung07288346@gmail.com>
2022-06-09 23:52:31 +0900

    📑 :: PostWeekendMealRequest

---
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/weekendmeal/presentation/dto/request/PostWeekendMealRequest.java
@@ -0,0 +1,16 @@
+package io.github.v1serviceapplication.domain.weekendmeal.presentation.dto.request;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostWeekendMealRequest {
+
+    @NotNull(message = "apply는 null일 수 없습니다.")
+    private Boolean apply;
+
+}
