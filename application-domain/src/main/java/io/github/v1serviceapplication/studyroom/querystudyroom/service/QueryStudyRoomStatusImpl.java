8efac441a0b466b3514d2b2d6747203eb7216183
leeseojune53 <sung07288346@gmail.com>
2022-06-14 19:12:28 +0900

    ⚡️ :: Null processing

--- application-domain/src/main/java/io/github/v1serviceapplication/studyroom/querystudyroom/service/QueryStudyRoomStatusImpl.java
+++ application-domain/src/main/java/io/github/v1serviceapplication/studyroom/querystudyroom/service/QueryStudyRoomStatusImpl.java
@@ -1,6 +1,7 @@
 package io.github.v1serviceapplication.studyroom.querystudyroom.service;
 
 import io.github.v1serviceapplication.annotation.DomainService;
+import io.github.v1serviceapplication.studyroom.StudyRoom;
 import io.github.v1serviceapplication.studyroom.querystudyroom.api.QueryStudyRoomStatus;
 import io.github.v1serviceapplication.studyroom.querystudyroom.spi.StudyRoomRepositorySpi;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +17,9 @@ public class QueryStudyRoomStatusImpl implements QueryStudyRoomStatus {
     @Override
     public UUID queryStudyRoomStatus() {
         return studyRoomRepositorySpi.findStudyRoomIdByUserId(
-                UUID.fromString("19d1e9b7-0d51-4405-bd1d-042cab403398")
-        ); //TODO userId
+                        UUID.fromString("19d1e9b7-0d51-4405-bd1d-042cab403398") // TODO userId
+                )
+                .map(StudyRoom::getId)
+                .orElse(null);
     }
 }
