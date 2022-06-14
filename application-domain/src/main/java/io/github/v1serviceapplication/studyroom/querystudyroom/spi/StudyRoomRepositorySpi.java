07a81741499ecb4ad4b4f05e9abd8ce807443d72
leeseojune53 <sung07288346@gmail.com>
2022-06-14 18:28:14 +0900

    ⚡️ :: findStudyRoomIdByUserId

--- application-domain/src/main/java/io/github/v1serviceapplication/studyroom/querystudyroom/spi/StudyRoomRepositorySpi.java
+++ application-domain/src/main/java/io/github/v1serviceapplication/studyroom/querystudyroom/spi/StudyRoomRepositorySpi.java
@@ -3,7 +3,9 @@ package io.github.v1serviceapplication.studyroom.querystudyroom.spi;
 import io.github.v1serviceapplication.studyroom.querystudyroom.spi.dto.StudyRoomModel;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface StudyRoomRepositorySpi {
     List<StudyRoomModel> findAll();
+    UUID findStudyRoomIdByUserId(UUID userId);
 }
