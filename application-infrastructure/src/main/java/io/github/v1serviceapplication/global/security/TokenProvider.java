3200e1e296b58609dd6c7b9f450283f5b6917a66
parksangwoo <b01056721617@gmail.com>
2022-06-15 09:17:40 +0900

    📑 :: (#104) add filter, provider

---
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/global/security/TokenProvider.java
@@ -0,0 +1,33 @@
+package io.github.v1serviceapplication.global.security;
+
+import io.github.v1serviceapplication.global.role.UserRole;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class TokenProvider {
+    private static final String TOKEN_HEADER = "Authorization";
+    private static final String TOKEN_PREFIX = "Bearer ";
+    private static final String AUTHORITY_KEY = "authorities";
+
+    public Authentication authenticateUser(String userId, UserRole userRole, List<String> userAuthorities) {
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        for (String aut : userAuthorities) {
+            authorities.add(new SimpleGrantedAuthority(aut));
+        }
+        authorities.add(new SimpleGrantedAuthority(userRole.name()));
+        UserDetails userDetails = new User(userId, "", authorities);
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+}
