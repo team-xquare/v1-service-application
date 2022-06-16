0d241b7648c19dee96bdd253c0bba7029bde67cd
leeseojune53 <sung07288346@gmail.com>
2022-06-16 23:39:12 +0900

    ⚡️ :: update, todayWeekMealApplyExist

--- application-domain/src/main/java/io/github/v1serviceapplication/weekendmeal/postweekendmeal/spi/PostWeekendMealApplyRepositorySpi.java
+++ application-domain/src/main/java/io/github/v1serviceapplication/weekendmeal/postweekendmeal/spi/PostWeekendMealApplyRepositorySpi.java
@@ -2,6 +2,10 @@ package io.github.v1serviceapplication.weekendmeal.postweekendmeal.spi;
 
 import io.github.v1serviceapplication.weekendmeal.WeekendMealApply;
 
+import java.util.UUID;
+
 public interface PostWeekendMealApplyRepositorySpi {
+    boolean todayWeekendMealApplyExist(UUID userId);
     void saveWeekendMealApply(WeekendMealApply weekendMealApply);
+    void updateWeekendMealApply(UUID userId, Boolean apply);
 }
--- application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/weekendmeal/domain/repository/CustomWeekendMealApplyRepositoryImpl.java
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/weekendmeal/domain/repository/CustomWeekendMealApplyRepositoryImpl.java
@@ -1,10 +1,18 @@
 package io.github.v1serviceapplication.domain.weekendmeal.domain.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import io.github.v1serviceapplication.domain.weekendmeal.domain.WeekendMealApplyEntity;
 import io.github.v1serviceapplication.domain.weekendmeal.mapper.WeekendMealApplyMapper;
 import io.github.v1serviceapplication.weekendmeal.WeekendMealApply;
+import io.github.v1serviceapplication.weekendmeal.exception.WeekendMealApplyNotFoundException;
 import io.github.v1serviceapplication.weekendmeal.postweekendmeal.spi.PostWeekendMealApplyRepositorySpi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.UUID;
+
+import static io.github.v1serviceapplication.domain.weekendmeal.domain.QWeekendMealApplyEntity.weekendMealApplyEntity;
 
 @RequiredArgsConstructor
 @Repository
@@ -12,11 +20,33 @@ public class CustomWeekendMealApplyRepositoryImpl implements PostWeekendMealAppl
 
     private final WeekendMealApplyRepository weekendMealApplyRepository;
     private final WeekendMealApplyMapper weekendMealApplyMapper;
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public boolean todayWeekendMealApplyExist(UUID userId) {
+        return jpaQueryFactory
+                .select(weekendMealApplyEntity.count())
+                .from(weekendMealApplyEntity)
+                .where(weekendMealApplyEntity.userId.eq(userId))
+                .orderBy(weekendMealApplyEntity.date.asc())
+                .limit(1)
+                .fetchFirst()
+                .equals(1L);
+    }
 
     @Override
     public void saveWeekendMealApply(WeekendMealApply weekendMealApply) {
         weekendMealApplyRepository.save(
                 weekendMealApplyMapper.domainToEntity(weekendMealApply)
         );
+    }
+
+    @Override
+    @Transactional
+    public void updateWeekendMealApply(UUID userId, Boolean apply) {
+        WeekendMealApplyEntity weekendMealApply =  weekendMealApplyRepository.findTop1ByUserIdOrderByDateAsc(userId)
+                .orElseThrow(() -> WeekendMealApplyNotFoundException.EXCEPTION);
+
+        weekendMealApply.updateApply(apply);
     }
 }
