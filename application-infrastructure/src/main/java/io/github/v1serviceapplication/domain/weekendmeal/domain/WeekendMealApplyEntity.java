5096e713ebc1a3c713c90bf9c3c1d24e8a21333b
leeseojune53 <sung07288346@gmail.com>
2022-06-16 16:29:35 +0900

    ⚡️ :: WeekendMealApply relation mapping

--- application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/weekendmeal/domain/WeekendMealApplyEntity.java
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/weekendmeal/domain/WeekendMealApplyEntity.java
@@ -14,6 +14,11 @@ import javax.validation.constraints.NotNull;
 @Table(name = "tbl_weekend_meal_apply")
 @Entity
 public class WeekendMealApplyEntity extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "weekend_meal_id")
+    private WeekendMealEntity weekendMeal;
+
     @NotNull
     @Column(columnDefinition = "TINYINT(1)")
     private Boolean isApplied;
