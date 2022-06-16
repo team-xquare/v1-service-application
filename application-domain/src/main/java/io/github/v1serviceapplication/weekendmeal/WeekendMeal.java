b637c4de66cf5cbc7e671ad82a36cf3c9ce28ac2
leeseojune53 <sung07288346@gmail.com>
2022-06-16 20:12:44 +0900

    📑 :: WeekendMeal Model

---
+++ application-domain/src/main/java/io/github/v1serviceapplication/weekendmeal/WeekendMeal.java
@@ -0,0 +1,17 @@
+package io.github.v1serviceapplication.weekendmeal;
+
+import io.github.v1serviceapplication.annotation.Aggregate;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Aggregate
+@Getter
+@Builder
+public class WeekendMeal {
+    private UUID id;
+    private String title;
+    private LocalDate date;
+}
