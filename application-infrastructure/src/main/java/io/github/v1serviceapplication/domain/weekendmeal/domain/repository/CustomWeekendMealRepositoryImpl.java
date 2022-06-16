bbb9b556e002ce540690e6abb97b1a6f551e968e
leeseojune53 <sung07288346@gmail.com>
2022-06-16 20:24:00 +0900

    📑 :: QueryWeekendMealRepositorySpi

---
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/weekendmeal/domain/repository/CustomWeekendMealRepositoryImpl.java
@@ -0,0 +1,32 @@
+package io.github.v1serviceapplication.domain.weekendmeal.domain.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import io.github.v1serviceapplication.domain.weekendmeal.mapper.WeekendMealMapper;
+import io.github.v1serviceapplication.weekendmeal.WeekendMeal;
+import io.github.v1serviceapplication.weekendmeal.queryweekendmeal.spi.QueryWeekendMealRepositorySpi;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+
+import static io.github.v1serviceapplication.domain.weekendmeal.domain.QWeekendMealEntity.weekendMealEntity;
+
+@RequiredArgsConstructor
+@Repository
+public class CustomWeekendMealRepositoryImpl implements QueryWeekendMealRepositorySpi {
+
+    private final JPAQueryFactory jpaQueryFactory;
+    private final WeekendMealMapper weekendMealMapper;
+
+    @Override
+    public WeekendMeal queryWeekendMealByDate() {
+        return weekendMealMapper.entityToDomain(
+                jpaQueryFactory
+                        .selectFrom(weekendMealEntity)
+                        .where(weekendMealEntity.date.after(LocalDate.now()))
+                        .orderBy(weekendMealEntity.date.asc())
+                        .fetchFirst()
+        );
+    }
+
+}
