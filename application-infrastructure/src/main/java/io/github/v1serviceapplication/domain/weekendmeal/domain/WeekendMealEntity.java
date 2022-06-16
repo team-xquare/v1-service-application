91239ba537078fa79b283772b27202f7a96fcbbc
leeseojune53 <sung07288346@gmail.com>
2022-06-16 16:29:04 +0900

    📑 :: WeekendMealEntity

---
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/weekendmeal/domain/WeekendMealEntity.java
@@ -0,0 +1,35 @@
+package io.github.v1serviceapplication.domain.weekendmeal.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "tbl_weekend_meal")
+@Entity
+public class WeekendMealEntity {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @Column(length = 20)
+    private String title;
+
+    private LocalDate date;
+
+}
