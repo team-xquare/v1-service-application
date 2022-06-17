0fa50e29347a4925a0eda9bd6b8c0e6a4116ab8f
leeseojune53 <sung07288346@gmail.com>
2022-06-17 17:51:19 +0900

    📑 :: QueryWeekendMealResponse

---
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/weekendmeal/presentation/dto/response/QueryWeekendMealResponse.java
@@ -0,0 +1,12 @@
+package io.github.v1serviceapplication.domain.weekendmeal.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class QueryWeekendMealResponse {
+
+    private final String title;
+
+}
