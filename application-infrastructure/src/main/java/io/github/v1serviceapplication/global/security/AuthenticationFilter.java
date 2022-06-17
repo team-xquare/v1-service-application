e6bb72a6d45e66bbf632b3c0ef29a61b13a611dc
parksangwoo <b01056721617@gmail.com>
2022-06-17 17:01:29 +0900

    ⚡️ :: (#108) add null check condition

--- application-infrastructure/src/main/java/io/github/v1serviceapplication/global/security/AuthenticationFilter.java
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/global/security/AuthenticationFilter.java
@@ -21,10 +21,10 @@ public class AuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if (request.getHeader("Request-User-Id") != null && request.getHeader("Request-User-Role") != null) {
+        if (request.getHeader("Request-User-Id") != null && request.getHeader("Request-User-Role") != null && request.getHeader("Request-User-Authorities") != null) {
             String userId = request.getHeader("Request-User-Id");
             UserRole userRole = UserRole.valueOf(request.getHeader("Request-User-Role"));
-            List<String> userAuthorities = request.getHeader("Request-User-Authorities") != null ? Collections.singletonList(request.getHeader("Request-User-Authorities")) : null;
+            List<String> userAuthorities = Collections.singletonList(request.getHeader("Request-User-Authorities"));
             Authentication authentication = tokenProvider.authenticateUser(userId, userRole, userAuthorities);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
