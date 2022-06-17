772e2831a27f422b35228b20da3633fbf08a41ae
leeseojune53 <sung07288346@gmail.com>
2022-06-17 18:20:38 +0900

    ⚡️ :: api merge

--- application-domain/src/main/java/io/github/v1serviceapplication/weekendmeal/queryweekendmeal/service/QueryWeekendMealImpl.java
+++ application-domain/src/main/java/io/github/v1serviceapplication/weekendmeal/queryweekendmeal/service/QueryWeekendMealImpl.java
@@ -4,24 +4,37 @@ import io.github.v1serviceapplication.annotation.DomainService;
 import io.github.v1serviceapplication.weekendmeal.WeekendMeal;
 import io.github.v1serviceapplication.weekendmeal.exception.WeekendMealNotFoundException;
 import io.github.v1serviceapplication.weekendmeal.queryweekendmeal.api.QueryWeekendMeal;
+import io.github.v1serviceapplication.weekendmeal.queryweekendmeal.api.dto.QueryWeekendMealResponse;
+import io.github.v1serviceapplication.weekendmeal.queryweekendmeal.spi.QueryWeekendMealApplyRepositorySpi;
 import io.github.v1serviceapplication.weekendmeal.queryweekendmeal.spi.QueryWeekendMealRepositorySpi;
 import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @DomainService
 public class QueryWeekendMealImpl implements QueryWeekendMeal {
 
     private final QueryWeekendMealRepositorySpi queryWeekendMealRepositorySpi;
+    private final QueryWeekendMealApplyRepositorySpi queryWeekendMealApplyRepositorySpi;
 
     @Override
-    public String queryWeekendMeal() {
+    public QueryWeekendMealResponse queryWeekendMeal() {
+        UUID userId = UUID.fromString("19d1e9b7-0d51-4405-bd1d-042cab403398");
+
         WeekendMeal weekendMeal = queryWeekendMealRepositorySpi.queryWeekendMealByDate();
 
-        if(weekendMeal == null) {
+        if (weekendMeal == null) {
             throw WeekendMealNotFoundException.EXCEPTION;
         }
 
-        return weekendMeal.getTitle();
+        boolean applied = queryWeekendMealApplyRepositorySpi
+                .queryWeekendMealApplyAppliedByUserIdAndWeekendMealId(userId, weekendMeal.getId());
+
+        return new QueryWeekendMealResponse(
+                weekendMeal.getTitle(),
+                applied
+        );
 
     }
 
