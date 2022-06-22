78143c298887138a1dd848a482f56a97e747c91b
parksangwoo <b01056721617@gmail.com>
2022-06-22 22:48:49 +0900

    ⚡️ :: (#118) add router

--- application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/stay/presentation/StayController.java
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/stay/presentation/StayController.java
@@ -1,8 +1,10 @@
 package io.github.v1serviceapplication.domain.stay.presentation;
 
 import io.github.v1serviceapplication.domain.stay.presentation.dto.request.ApplyStayRequest;
+import io.github.v1serviceapplication.domain.stay.presentation.dto.request.SetDefaultStayRequest;
 import io.github.v1serviceapplication.stay.api.ApplyStay;
 import io.github.v1serviceapplication.stay.api.QueryStayStatus;
+import io.github.v1serviceapplication.stay.api.SetDefaultStay;
 import io.github.v1serviceapplication.stay.api.dto.response.QueryStayStatusResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,7 @@ import java.util.UUID;
 @RequestMapping("/stay")
 public class StayController {
 
+    private final SetDefaultStay setDefaultStay;
     private final ApplyStay applyStay;
     private final QueryStayStatus queryStayStatus;
 
@@ -26,5 +29,11 @@ public class StayController {
     @GetMapping
     public QueryStayStatusResponse queryStayStatus(@RequestHeader("userId") String userId) {
         return queryStayStatus.queryStayStatus(UUID.fromString(userId));  // todo header에서 userId 받아서 할당
+    }
+
+    @PostMapping("/signup")
+    public void setDefaultStay(@RequestBody @Valid SetDefaultStayRequest request) {
+        System.out.println(request.getUserId());
+        setDefaultStay.setDefaultStay(request.getUserId());
     }
 }
