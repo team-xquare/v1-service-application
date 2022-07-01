5c5c8629d75d678aecdac0bc021f76a4f105bee4
leeseojune53 <sung07288346@gmail.com>
2022-07-01 17:20:30 +0900

    ⚡️ :: StudyController

--- application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/studyroom/presentation/StudyController.java
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/studyroom/presentation/StudyController.java
@@ -12,10 +12,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/study-room")
@@ -27,8 +29,8 @@ public class StudyController {
     private final QueryStudyRoomStatus queryStudyRoomStatus;
 
     @GetMapping
-    public StudyRoomList queryStudyRoomList() {
-        return new StudyRoomList(queryStudyRoom.queryStudyRooms());
+    public StudyRoomList queryStudyRoomList(@RequestParam("floor") List<Integer> floorList) {
+        return new StudyRoomList(queryStudyRoom.queryStudyRooms(floorList));
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
