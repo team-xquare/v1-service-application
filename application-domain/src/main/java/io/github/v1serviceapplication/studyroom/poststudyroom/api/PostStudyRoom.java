100522da979531d663546c2226d1353f2d6ba62f
leeseojune53 <sung07288346@gmail.com>
2022-08-24 17:27:47 +0900

    ⚰️ :: 사용하지 않는 클래스

--- application-domain/src/main/java/io/github/v1serviceapplication/studyroom/poststudyroom/api/PostStudyRoom.java
+++
@@ -1,7 +0,0 @@
-package io.github.v1serviceapplication.studyroom.poststudyroom.api;
-
-import java.util.UUID;
-
-public interface PostStudyRoom {
-    void postStudyRoom(UUID studyRoomId, UUID userId);
-}
--- application-domain/src/main/java/io/github/v1serviceapplication/studyroom/querystudyroom/api/QueryStudyRoom.java
+++
@@ -1,9 +0,0 @@
-package io.github.v1serviceapplication.studyroom.querystudyroom.api;
-
-import io.github.v1serviceapplication.studyroom.querystudyroom.api.dto.response.StudyRoomElement;
-
-import java.util.List;
-
-public interface QueryStudyRoom {
-    List<StudyRoomElement> queryStudyRooms();
-}
