643d2dc0d4ea31f6e8d72eff9a09297518230285
leeseojune53 <sung07288346@gmail.com>
2022-08-31 23:37:40 +0900

    ⚰️ :: unused Import

--- application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/stay/presentation/StayController.java
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/stay/presentation/StayController.java
@@ -1,10 +1,6 @@
 package io.github.v1serviceapplication.domain.stay.presentation;
 
 import io.github.v1serviceapplication.domain.stay.presentation.dto.request.ApplyStayRequest;
-import io.github.v1serviceapplication.domain.stay.presentation.dto.request.SetDefaultStayRequest;
-import io.github.v1serviceapplication.stay.api.ApplyStay;
-import io.github.v1serviceapplication.stay.api.QueryStayStatus;
-import io.github.v1serviceapplication.stay.api.SetDefaultStay;
 import io.github.v1serviceapplication.stay.api.StayApi;
 import io.github.v1serviceapplication.stay.api.dto.response.QueryStayStatusResponse;
 import lombok.RequiredArgsConstructor;
