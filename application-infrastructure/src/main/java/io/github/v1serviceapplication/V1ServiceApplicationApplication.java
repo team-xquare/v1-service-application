d6322078672cfcb5fc117ff4888faffe5725faa1
parksangwoo <b01056721617@gmail.com>
2022-09-13 21:49:39 +0900

    ⚡️ :: @EnableSwagger2 어노테이션 추가

--- application-infrastructure/src/main/java/io/github/v1serviceapplication/V1ServiceApplicationApplication.java
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/V1ServiceApplicationApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableJpaAuditing
+@EnableSwagger2
 public class V1ServiceApplicationApplication {
     public static void main(String[] args) {
         SpringApplication.run(V1ServiceApplicationApplication.class, args);
