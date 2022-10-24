aa4c470ba8197f49dc3fc81d933f276b9432bc20
leeseojune53 <sung07288346@gmail.com>
2022-10-25 00:07:52 +0900

    📑 :: UserInfoResponse

---
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/infrastructure/feign/client/dto/response/UserInfoResponse.java
@@ -0,0 +1,15 @@
+package io.github.v1serviceapplication.infrastructure.feign.client.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class UserInfoResponse {
+
+    private List<UserInfoResponseElement> users;
+
+}
