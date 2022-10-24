7e576300cb313236b4553b094d5787c607554921
leeseojune53 <sung07288346@gmail.com>
2022-10-24 23:08:54 +0900

    📑 :: findTodayByStudyRoomId

---
+++ application-domain/src/main/java/io/github/v1serviceapplication/studyroom/spi/StudyRoomQueryExtensionRepositorySpi.java
@@ -0,0 +1,8 @@
+package io.github.v1serviceapplication.studyroom.spi;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface StudyRoomQueryExtensionRepositorySpi {
+    List<UUID> findTodayByStudyRoomId(UUID studyRoomId);
+}
