5c626615b256e57bf73ce26d66335365a2ab6245
leeseojune53 <sung07288346@gmail.com>
2022-10-25 17:46:36 +0900

    🧪 :: Student Test

--- application-domain/src/test/java/io/github/v1serviceapplication/studyroom/querystudyroom/service/QueryStudyRoomImplTest.java
+++ application-domain/src/test/java/io/github/v1serviceapplication/studyroom/querystudyroom/service/QueryStudyRoomImplTest.java
@@ -2,13 +2,17 @@ package io.github.v1serviceapplication.studyroom.querystudyroom.service;
 
 import io.github.v1serviceapplication.common.UserIdFacade;
 import io.github.v1serviceapplication.studyroom.api.StudyRoomApi;
+import io.github.v1serviceapplication.studyroom.api.dto.response.StudentElement;
 import io.github.v1serviceapplication.studyroom.extension.Extension;
 import io.github.v1serviceapplication.stubs.InMemoryQueryStudyRoomRepository;
 import io.github.v1serviceapplication.studyroom.StudyRoom;
 import io.github.v1serviceapplication.studyroom.service.StudyRoomApiImpl;
 import io.github.v1serviceapplication.studyroom.spi.PostStudyRoomRepositorySpi;
+import io.github.v1serviceapplication.studyroom.spi.StudyRoomQueryExtensionRepositorySpi;
+import io.github.v1serviceapplication.studyroom.spi.StudyRoomUserFeignSpi;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -18,13 +22,25 @@ class QueryStudyRoomImplTest {
 
     private final PostStudyRoomRepositorySpi postStudyRoomRepositorySpi = mock(PostStudyRoomRepositorySpi.class);
     private final InMemoryQueryStudyRoomRepository studyRoomRepositorySpi = new InMemoryQueryStudyRoomRepository();
+    private final StudyRoomQueryExtensionRepositorySpi studyRoomQueryExtensionRepositorySpi = new StudyRoomQueryExtensionRepositorySpi() {
+        @Override
+        public List<UUID> findStudentIdByRoomIdAndToday(UUID studyRoomId) {
+            return List.of(UUID.randomUUID());
+        }
+    };
+    private final StudyRoomUserFeignSpi studyRoomUserFeignSpi = new StudyRoomUserFeignSpi() {
+        @Override
+        public List<StudentElement> queryUserInfoByUserId(List<UUID> userId) {
+            return List.of(new StudentElement("Test", "Test Image path"));
+        }
+    };
     private final UserIdFacade userIdFacade = new UserIdFacade() {
         @Override
         public UUID getCurrentUserId() {
             return null;
         }
     };
-    private final StudyRoomApi studyRoomApi = new StudyRoomApiImpl(postStudyRoomRepositorySpi, studyRoomRepositorySpi, userIdFacade);
+    private final StudyRoomApi studyRoomApi = new StudyRoomApiImpl(postStudyRoomRepositorySpi, studyRoomRepositorySpi, studyRoomQueryExtensionRepositorySpi, studyRoomUserFeignSpi, userIdFacade);
 
     @Test
     void queryStudyRooms() {
@@ -50,7 +66,10 @@ class QueryStudyRoomImplTest {
                     assertThat(queryStudy.getId()).isEqualTo(studyRoomId);
                     assertThat(queryStudy.getStudyRoomName()).isEqualTo(studyRoomName);
                     assertThat(queryStudy.getApplicationCount()).isEqualTo(1);
-                    queryStudy.getStudents().forEach(id -> assertThat(id).isEqualTo(userId));
+                    queryStudy.getStudents().forEach(it -> {
+                        assertThat(it.getStudentName()).isEqualTo("Test");
+                        assertThat(it.getProfileImage()).isEqualTo("Test Image path");
+                    });
                 });
 
     }
