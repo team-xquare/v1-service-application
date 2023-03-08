300e547b40050d88298d02fafaf60c5a710b73db
lyutvs <lyutvs@gmail.com>
2023-03-08 19:34:05 +0900

    ⚡️ :: repository List 타입으로 변경

--- application-domain/src/main/java/io/github/v1serviceapplication/picnic/spi/PicnicRepositorySpi.java
+++ application-domain/src/main/java/io/github/v1serviceapplication/picnic/spi/PicnicRepositorySpi.java
@@ -3,7 +3,6 @@ package io.github.v1serviceapplication.picnic.spi;
 import io.github.v1serviceapplication.annotation.Spi;
 import io.github.v1serviceapplication.picnic.Picnic;
 import io.github.v1serviceapplication.picnic.api.dto.PicnicUserElement;
-import io.github.v1serviceapplication.stay.api.dto.response.StayUserElement;
 
 import java.util.List;
 import java.util.Optional;
@@ -27,7 +26,7 @@ public interface PicnicRepositorySpi {
 
     void refusePicnic(UUID picnicId);
 
-    Optional<Picnic> findByUserId(UUID userId);
+    List<Picnic> findAllByUserIdAndIsAcceptance(UUID userId);
 
     PicnicUserElement getUserInfo(UUID userId);
 }
