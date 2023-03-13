4148cb0fc10a7201be01af9abbae74260f782a57
jaemin <sonjm3518@dsm.hs.kr>
2023-03-13 19:46:29 +0900

    ♻️ :: for문으로 정리하기

--- application-infrastructure/src/main/java/io/github/v1serviceapplication/infrastructure/excel/StayStatusExcel.java
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/infrastructure/excel/StayStatusExcel.java
@@ -54,88 +54,32 @@ public class StayStatusExcel {
             int columnNo = 4 * cls - 3; // 1
 
             Row row = sheet.getRow(rowNo) == null ? sheet.createRow(rowNo) : sheet.getRow(rowNo);
-            Cell cell1 = row.createCell(columnNo++);
-            cell1.setCellValue(stayStatus.getNum());
-            cell1.setCellStyle(cellStyle);
+            Cell studentId = row.createCell(columnNo++);
+            studentId.setCellValue(stayStatus.getNum());
+            studentId.setCellStyle(cellStyle);
 
-            Cell cell2 = row.createCell(columnNo++);
-            cell2.setCellValue(stayStatus.getName());
-            cell2.setCellStyle(cellStyle);
+            Cell name = row.createCell(columnNo++);
+            name.setCellValue(stayStatus.getName());
+            name.setCellStyle(cellStyle);
 
-            Cell cell3 = row.createCell(columnNo);
-            cell3.setCellValue(stayStatus.getStay());
-            cell3.setCellStyle(cellStyle);
+            Cell status = row.createCell(columnNo);
+            status.setCellValue(stayStatus.getStay());
+            status.setCellStyle(cellStyle);
 
-            Cell cell4 = row.createCell(columnNo + 1);
-            cell4.setCellStyle(cellStyle);
+            Cell sign = row.createCell(columnNo + 1);
+            sign.setCellStyle(cellStyle);
         }
 
         return workbook;
     }
 
     private void setHeaderRow(Row row, CellStyle cellStyle) {
-        Cell cell1 = row.createCell(1);
-        cell1.setCellValue("학번");
-        cell1.setCellStyle(cellStyle);
-
-        Cell cell2 = row.createCell(2);
-        cell2.setCellValue("이름");
-        cell2.setCellStyle(cellStyle);
-
-        Cell cell3 = row.createCell(3);
-        cell3.setCellValue("잔류현황");
-        cell3.setCellStyle(cellStyle);
-
-        Cell cell4 = row.createCell(4);
-        cell4.setCellValue("서명");
-        cell4.setCellStyle(cellStyle);
-
-        Cell cell5 = row.createCell(5);
-        cell5.setCellValue("학번");
-        cell5.setCellStyle(cellStyle);
-
-        Cell cell6 = row.createCell(6);
-        cell6.setCellValue("이름");
-        cell6.setCellStyle(cellStyle);
-
-        Cell cell7 = row.createCell(7);
-        cell7.setCellValue("잔류현황");
-        cell7.setCellStyle(cellStyle);
-
-        Cell cell8 = row.createCell(8);
-        cell8.setCellValue("서명");
-        cell8.setCellStyle(cellStyle);
-
-        Cell cell9 = row.createCell(9);
-        cell9.setCellValue("학번");
-        cell9.setCellStyle(cellStyle);
-
-        Cell cell10 = row.createCell(10);
-        cell10.setCellValue("이름");
-        cell10.setCellStyle(cellStyle);
-
-        Cell cell11 = row.createCell(11);
-        cell11.setCellValue("잔류현황");
-        cell11.setCellStyle(cellStyle);
-
-        Cell cell12 = row.createCell(12);
-        cell12.setCellValue("서명");
-        cell12.setCellStyle(cellStyle);
-
-        Cell cell13 = row.createCell(13);
-        cell13.setCellValue("학번");
-        cell13.setCellStyle(cellStyle);
-
-        Cell cell14 = row.createCell(14);
-        cell14.setCellValue("이름");
-        cell14.setCellStyle(cellStyle);
-
-        Cell cell15 = row.createCell(15);
-        cell15.setCellValue("잔류현황");
-        cell15.setCellStyle(cellStyle);
-
-        Cell cell16 = row.createCell(16);
-        cell16.setCellValue("서명");
-        cell16.setCellStyle(cellStyle);
+        String[] header = new String[]{"학번", "이름", "잔류현황", "서명"};
+        for (int i = 1; i <= 16; i++) {
+            Cell cell = row.createCell(i);
+            int order = i % 4 == 0 ? 3 : i % 4 - 1;
+            cell.setCellValue(header[order]);
+            cell.setCellStyle(cellStyle);
+        }
     }
 }
