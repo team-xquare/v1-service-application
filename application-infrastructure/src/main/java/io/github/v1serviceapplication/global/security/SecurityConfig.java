542ea296de905915f10c7ae002538449805110ef
KimSeungJin <klli1naver@gmail.com>
2023-03-14 19:48:01 +0900

    📑 :: 권한 추가

--- application-infrastructure/src/main/java/io/github/v1serviceapplication/global/security/SecurityConfig.java
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/global/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package io.github.v1serviceapplication.global.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.v1serviceapplication.global.role.UserRole;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -16,6 +17,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final ObjectMapper objectMapper;
 
+
+    private static final String STUDENT = "ROLE_" + UserRole.STU.name();
+    private static final String SCHOOL = "ROLE_" + UserRole.SCH.name();
+    private static final String DORMITORY = "ROLE_" + UserRole.DOR.name();
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
@@ -26,32 +32,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 .authorizeRequests()
                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-                .antMatchers(HttpMethod.GET, "/study-room").permitAll()
-                .antMatchers(HttpMethod.POST, "/study-room").permitAll()
-                .antMatchers(HttpMethod.DELETE, "/study-room/{study_room_id}").permitAll()
-                .antMatchers(HttpMethod.GET, "/study-room/status").permitAll()
-                .antMatchers(HttpMethod.GET, "/weekend-meal").permitAll()
-                .antMatchers(HttpMethod.POST, "/weekend-meal").permitAll()
-                .antMatchers(HttpMethod.POST, "/picnic").permitAll()
-                .antMatchers(HttpMethod.PUT, "/stay").permitAll()
-                .antMatchers(HttpMethod.GET, "/stay").permitAll()
-                .antMatchers(HttpMethod.GET, "/stay/codes/status").permitAll()
-                .antMatchers(HttpMethod.POST, "/stay/signup").permitAll()
-                .antMatchers(HttpMethod.DELETE, "/stay/signup/{user-uuid}").permitAll()
-                .antMatchers(HttpMethod.GET, "/admin/weekend-meal").permitAll()
-                .antMatchers(HttpMethod.GET, "/admin/picnic").permitAll()
-                .antMatchers(HttpMethod.GET, "/admin/stay").permitAll()
-                .antMatchers(HttpMethod.GET, "/weekend-meal/excel").permitAll()
-                .antMatchers(HttpMethod.PATCH, "/admin/picnic/arrive/{picnic-id}").permitAll()
-                .antMatchers(HttpMethod.PATCH, "/admin/picnic/accept/{picnic-id}").permitAll()
-                .antMatchers(HttpMethod.DELETE, "/admin/picnic/refuse/{picnic-id}").permitAll()
-                .antMatchers(HttpMethod.GET, "/admin/stay/{student-id}").permitAll()
-                .antMatchers(HttpMethod.PUT, "/admin/stay/{student-id}").permitAll()
-                .antMatchers(HttpMethod.GET, "/stay/list").permitAll()
-                .antMatchers(HttpMethod.GET, "/stay/excel").permitAll()
-                .antMatchers(HttpMethod.GET, "/weekend-meal/excel").permitAll()
-                .antMatchers(HttpMethod.GET, "/picnic/excel").permitAll()
-                .antMatchers(HttpMethod.GET, "/admin/picnic/detail/{picnic-id}").permitAll()
+                .antMatchers(HttpMethod.GET, "/study-room").hasAnyRole(STUDENT, SCHOOL)
+                .antMatchers(HttpMethod.POST, "/study-room").hasAnyRole(STUDENT, SCHOOL)
+                .antMatchers(HttpMethod.DELETE, "/study-room/{study_room_id}").hasAnyRole(STUDENT, SCHOOL)
+                .antMatchers(HttpMethod.GET, "/study-room/status").hasAnyRole(STUDENT, SCHOOL)
+                .antMatchers(HttpMethod.GET, "/weekend-meal").hasAnyRole(STUDENT, SCHOOL)
+                .antMatchers(HttpMethod.POST, "/weekend-meal").hasAnyRole(STUDENT, SCHOOL)
+                .antMatchers(HttpMethod.POST, "/picnic").hasAnyRole(STUDENT, SCHOOL)
+                .antMatchers(HttpMethod.PUT, "/stay").hasAnyRole(STUDENT, SCHOOL)
+                .antMatchers(HttpMethod.GET, "/stay").hasAnyRole(STUDENT, SCHOOL)
+                .antMatchers(HttpMethod.GET, "/stay/codes/status").hasAnyRole(STUDENT, SCHOOL)
+                .antMatchers(HttpMethod.POST, "/stay/signup").hasAnyRole(STUDENT, SCHOOL)
+                .antMatchers(HttpMethod.DELETE, "/stay/signup/{user-uuid}").hasAnyRole(STUDENT, SCHOOL)
+                .antMatchers(HttpMethod.GET, "/admin/weekend-meal").hasAnyRole(DORMITORY)
+                .antMatchers(HttpMethod.GET, "/admin/picnic").hasAnyRole(DORMITORY)
+                .antMatchers(HttpMethod.GET, "/admin/stay").hasAnyRole(DORMITORY)
+                .antMatchers(HttpMethod.GET, "/weekend-meal/excel").hasAnyRole(DORMITORY)
+                .antMatchers(HttpMethod.PATCH, "/admin/picnic/arrive/{picnic-id}").hasAnyRole(DORMITORY)
+                .antMatchers(HttpMethod.PATCH, "/admin/picnic/accept/{picnic-id}").hasAnyRole(DORMITORY)
+                .antMatchers(HttpMethod.DELETE, "/admin/picnic/refuse/{picnic-id}").hasAnyRole(DORMITORY)
+                .antMatchers(HttpMethod.GET, "/admin/stay/{student-id}").hasAnyRole(DORMITORY)
+                .antMatchers(HttpMethod.PUT, "/admin/stay/{student-id}").hasAnyRole(DORMITORY)
+                .antMatchers(HttpMethod.GET, "/stay/list").hasAnyRole(DORMITORY)
+                .antMatchers(HttpMethod.GET, "/stay/excel").hasAnyRole(DORMITORY)
+                .antMatchers(HttpMethod.GET, "/weekend-meal/excel").hasAnyRole(DORMITORY)
+                .antMatchers(HttpMethod.GET, "/picnic/excel").hasAnyRole(DORMITORY)
+                .antMatchers(HttpMethod.GET, "/admin/picnic/detail/{picnic-id}").hasAnyRole(DORMITORY)
                 .antMatchers("/swagger-ui/**").permitAll()
                 .antMatchers("/docs/**").permitAll()
                 .anyRequest().authenticated()
