1eac8a744765485379722fb6205c4be11a674e4b
min56 <minchaeg85@gmail.com>
2023-03-20 19:12:04 +0900

    ♻️ :: 메소드 분리

--- application-infrastructure/src/main/java/io/github/v1serviceapplication/infrastructure/excel/WeekendPicnicExcel.java
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/infrastructure/excel/WeekendPicnicExcel.java
@@ -26,18 +26,31 @@ public class WeekendPicnicExcel {
         cellStyle.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
 
         Sheet sheet = workbook.createSheet("주말외출현황");
+        Sheet sheet2 = workbook.createSheet("주말외출대기현황");
+
+        List<WeekendPicnicExcelElement> weekendPicnicList =
+                picnicApi.weekendPicnicExcel().getWeekendPicnicList().stream().filter(picnic -> picnic.getIsAcceptance() == true).toList();
+
+        List<WeekendPicnicExcelElement> weekendPicnicAwaitList =
+                picnicApi.weekendPicnicExcel().getWeekendPicnicList().stream().filter(picnic -> picnic.getIsAcceptance() == false).toList();
+
+        createWorkBook(workbook, sheet, weekendPicnicList);
+        createWorkBook(workbook, sheet2, weekendPicnicAwaitList);
+
+        return workbook;
+    }
+
+    public void createWorkBook(Workbook workbook, Sheet sheet, List<WeekendPicnicExcelElement> weekendPicnicAwaitList) {
+        CellStyle cellStyle = workbook.createCellStyle();
+        cellStyle.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
 
         Row locationRow = sheet.createRow(0);
 
         setHeaderRow(locationRow);
 
-        List<WeekendPicnicExcelElement> weekendPicnicList =
-                picnicApi.weekendPicnicExcel().getWeekendPicnicList().stream().filter(picnic -> picnic.getIsAcceptance() == true).toList();
-
         int cellNum = 1;
         int rowNum = 2;
-
-        for (WeekendPicnicExcelElement weekendPicnicExcelElement : weekendPicnicList) {
+        for (WeekendPicnicExcelElement weekendPicnicExcelElement : weekendPicnicAwaitList) {
             Row row = sheet.createRow(rowNum);
             row.createCell(cellNum).setCellValue(weekendPicnicExcelElement.getName());
             row.createCell(cellNum + 1).setCellValue(weekendPicnicExcelElement.getNum());
@@ -45,45 +58,14 @@ public class WeekendPicnicExcel {
             row.createCell(cellNum + 3).setCellValue(String.valueOf(weekendPicnicExcelElement.getEndTime()));
             row.createCell(cellNum + 4).setCellValue(weekendPicnicExcelElement.getReason());
             row.createCell(cellNum + 5).setCellValue(weekendPicnicExcelElement.getArrangement());
-
+            rowNum++;
         }
-
-        createWeekendOutingWaitingListWorkBook(workbook);
-        return workbook;
-    }
-
-    public Workbook createWeekendOutingWaitingListWorkBook(Workbook workbook) {
-        CellStyle cellStyle = workbook.createCellStyle();
-        cellStyle.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
-
-        Sheet sheet2 = workbook.createSheet("주말외출대기현황");
-
-        Row locationRow = sheet2.createRow(0);
-
-        setHeaderRow(locationRow);
-
-        List<WeekendPicnicExcelElement> weekendPicnicList =
-                picnicApi.weekendPicnicExcel().getWeekendPicnicList().stream().filter(picnic -> picnic.getIsAcceptance() == false).toList();
-
-        int cellNum = 1;
-        int rowNum = 2;
-        for (WeekendPicnicExcelElement weekendPicnicExcelElement : weekendPicnicList) {
-            Row row = sheet2.createRow(rowNum);
-            row.createCell(cellNum).setCellValue(weekendPicnicExcelElement.getName());
-            row.createCell(cellNum + 1).setCellValue(weekendPicnicExcelElement.getNum());
-            row.createCell(cellNum + 2).setCellValue(String.valueOf(weekendPicnicExcelElement.getStartTime()));
-            row.createCell(cellNum + 3).setCellValue(String.valueOf(weekendPicnicExcelElement.getEndTime()));
-            row.createCell(cellNum + 4).setCellValue(weekendPicnicExcelElement.getReason());
-            row.createCell(cellNum + 5).setCellValue(weekendPicnicExcelElement.getArrangement());
-
-        }
-        return workbook;
 
     }
 
     private void setHeaderRow(Row row) {
         String[] cellValues = {"이름", "학번", "외출시간", "귀가시간", "외출사유", "동행인"};
-        int number = 0;
+        int number = 1;
         for (String values : cellValues) {
             row.createCell(number++).setCellValue(values);
         }
