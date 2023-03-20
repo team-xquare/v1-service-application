1343d7b2f6b3f8baaf92d5a9f46aaadee8e82e38
jaemin <sonjm3518@dsm.hs.kr>
2023-03-20 22:27:51 +0900

    📑 :: 주말 외출 예약 Entity

---
+++ application-domain/src/main/java/io/github/v1serviceapplication/reservation/PicnicReservation.java
@@ -0,0 +1,17 @@
+package io.github.v1serviceapplication.reservation;
+
+import io.github.v1serviceapplication.annotation.Aggregate;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Aggregate
+@Getter
+@Builder
+public class PicnicReservation {
+    private final UUID id;
+    private final UUID userId;
+    private final LocalDate date;
+}
---
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/reservation/domain/repository/PicnicReservationRepository.java
@@ -0,0 +1,14 @@
+package io.github.v1serviceapplication.domain.reservation.domain.repository;
+
+import io.github.v1serviceapplication.domain.reservation.domain.PicnicReservationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.UUID;
+import java.util.List;
+
+@Repository
+public interface PicnicReservationRepository extends JpaRepository<PicnicReservationEntity, UUID> {
+    List<PicnicReservationEntity> findAllByDate(LocalDate localDate);
+}
