d2a95f710566719b7b8e916126399bc111add3cb
jaemin <sonjm3518@dsm.hs.kr>
2023-03-21 11:05:18 +0900

    ♻️ :: public → private

--- application-domain/src/main/java/io/github/v1serviceapplication/error/UserNotEmptyException.java
+++ application-domain/src/main/java/io/github/v1serviceapplication/error/UserNotEmptyException.java
@@ -3,7 +3,7 @@ package io.github.v1serviceapplication.error;
 public class UserNotEmptyException extends ApplicationException {
 
     public static final ApplicationException EXCEPTION = new UserNotEmptyException();
-    public UserNotEmptyException() {
-        super(ErrorCode.USER_ID_EXIST);
+    private UserNotEmptyException() {
+        super(ErrorCode.USER_NOT_EMPTY);
     }
 }
