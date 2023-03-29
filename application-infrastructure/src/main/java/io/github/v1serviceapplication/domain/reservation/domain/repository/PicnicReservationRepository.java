78b1827ce14d71315aeaede0b0c8c2aaef53984f
jaemin <sonjm3518@dsm.hs.kr>
2023-03-29 11:42:35 +0900

    ♻️ :: 동시성 문제로 인한 Lock 걸기

--- application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/reservation/domain/repository/PicnicReservationRepository.java
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/reservation/domain/repository/PicnicReservationRepository.java
@@ -3,8 +3,10 @@ package io.github.v1serviceapplication.domain.reservation.domain.repository;
 import io.github.v1serviceapplication.domain.reservation.domain.PicnicReservationEntity;
 import io.github.v1serviceapplication.reservation.PicnicReservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.LockModeType;
 import java.time.LocalDate;
 import java.util.Optional;
 import java.util.UUID;
@@ -16,5 +18,6 @@ public interface PicnicReservationRepository extends JpaRepository<PicnicReserva
 
     boolean existsByUserIdAndDate(UUID userId, LocalDate date);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<PicnicReservationEntity> findByUserIdAndDate(UUID userId, LocalDate date);
 }
