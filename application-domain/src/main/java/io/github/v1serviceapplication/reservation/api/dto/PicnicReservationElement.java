3671081ad576dd6543ca6d786d02b0a3e9edc697
jaemin <sonjm3518@dsm.hs.kr>
2023-04-06 19:46:23 +0900

    ♻️ :: id 제거

--- application-domain/src/main/java/io/github/v1serviceapplication/reservation/api/dto/PicnicReservationElement.java
+++ application-domain/src/main/java/io/github/v1serviceapplication/reservation/api/dto/PicnicReservationElement.java
@@ -3,12 +3,9 @@ package io.github.v1serviceapplication.reservation.api.dto;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.util.UUID;
-
 @Getter
 @Builder
 public class PicnicReservationElement {
-    private final UUID id;
     private final String num;
     private final String name;
     private final boolean reserved;
