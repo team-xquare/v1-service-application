39e4de238c2fc1371eeb379dd20f7f78da1218e2
min56 <minchaeg85@gmail.com>
2023-04-25 12:36:15 +0900

    📑 :: PicnicDateTimeEntity 추가

---
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/picnicdatetime/domain/PicnicDateTimeEntity.java
@@ -0,0 +1,42 @@
+package io.github.v1serviceapplication.domain.picnicdatetime.domain;
+
+import io.github.v1serviceapplication.picnicdatetime.DateTimeType;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Table(name = "tbl_picnic_date_time")
+@Entity
+public class PicnicDateTimeEntity {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @NotNull
+    private LocalDateTime picnicDateTime;
+
+    @Enumerated(EnumType.ORDINAL)
+    @NotNull
+    private DateTimeType dateTimeType;
+}
