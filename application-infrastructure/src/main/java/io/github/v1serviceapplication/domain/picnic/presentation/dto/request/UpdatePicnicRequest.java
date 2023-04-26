604637f96f1a15bbf21b2746fe14b1ee6ef0b3cf
min56 <minchaeg85@gmail.com>
2023-04-26 13:08:36 +0900

    ♻️ :: @NoArgsConstructor 추가

--- application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/picnic/presentation/dto/request/UpdatePicnicRequest.java
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/picnic/presentation/dto/request/UpdatePicnicRequest.java
@@ -1,9 +1,11 @@
 package io.github.v1serviceapplication.domain.picnic.presentation.dto.request;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalTime;
 
+@NoArgsConstructor
 @Getter
 public class UpdatePicnicRequest {
     private LocalTime startTime;
