4d485ee64f40fa3c78564ecefecc361d36c08781
lyutvs <lyutvs@gmail.com>
2023-05-07 22:44:17 +0900

    📑 :: AmazonSqsConfig

---
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/global/config/AmazonSqsConfig.java
@@ -0,0 +1,33 @@
+package io.github.v1serviceapplication.global.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class AmazonSqsConfig {
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Primary
+    @Bean
+    public AmazonSQSAsync amazonSQSAws() {
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonSQSAsyncClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .build();
+    }
+}
