a4171859b1dcaebbd10230a84bc6192458148ba5
jaemin <sonjm3518@dsm.hs.kr>
2023-05-12 01:31:46 +0900

    ⚡ :: 주말 외출 예약 현황 엑셀 다운로드

--- application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/reservation/presentation/PicnicReservationController.java
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/reservation/presentation/PicnicReservationController.java
@@ -1,14 +1,18 @@
 package io.github.v1serviceapplication.domain.reservation.presentation;
 
 import io.github.v1serviceapplication.domain.reservation.presentation.dto.request.ReserveWeekendPicnicRequest;
+import io.github.v1serviceapplication.infrastructure.excel.PicnicReservationExel;
 import io.github.v1serviceapplication.reservation.api.PicnicReservationApi;
 import io.github.v1serviceapplication.reservation.api.dto.PicnicReservationListResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.IOException;
 
 @Tag(name = "주말 외출 예약 API")
 @RestController
@@ -17,6 +21,7 @@ import javax.validation.Valid;
 public class PicnicReservationController {
 
     private final PicnicReservationApi picnicReservationApi;
+    private final PicnicReservationExel picnicReservationExel;
 
     @Operation(summary = "주말 외출 예약 및 업데이트 API")
     @PostMapping
@@ -28,5 +33,19 @@ public class PicnicReservationController {
     @GetMapping
     public PicnicReservationListResponse getWeekendPicnicReservation() {
         return picnicReservationApi.getPicnicReservationList();
+    }
+
+    @Operation(summary = "주말 외출 예약자 현황 엑셀 다운로드 API")
+    @GetMapping("/excel")
+    public void excel(HttpServletResponse response) throws IOException {
+        Workbook workbook = picnicReservationExel.createWorkHook();
+
+        String filename = "주말외출예약자.xlsx";
+
+        response.setContentType("ms-vnd/excel");
+        response.setHeader("Content-Disposition", "attachment;filename=" + new String(filename.getBytes("KSC5601"), "8859_1"));
+
+        workbook.write(response.getOutputStream());
+        workbook.close();
     }
 }
