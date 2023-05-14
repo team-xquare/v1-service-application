6563809b0efa6b7a392619bbe5b41a2f477db0c9
jaemin <sonjm3518@dsm.hs.kr>
2023-05-14 22:18:34 +0900

    ⚰️ :: 불필요한 코드 제거

--- application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/reservation/presentation/PicnicReservationController.java
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/reservation/presentation/PicnicReservationController.java
@@ -1,7 +1,6 @@
 package io.github.v1serviceapplication.domain.reservation.presentation;
 
 import io.github.v1serviceapplication.domain.reservation.presentation.dto.request.ReserveWeekendPicnicRequest;
-import io.github.v1serviceapplication.infrastructure.excel.PicnicReservationExel;
 import io.github.v1serviceapplication.reservation.api.PicnicReservationApi;
 import io.github.v1serviceapplication.reservation.api.dto.PicnicReservationListResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -21,7 +20,6 @@ import java.io.IOException;
 public class PicnicReservationController {
 
     private final PicnicReservationApi picnicReservationApi;
-    private final PicnicReservationExel picnicReservationExel;
 
     @Operation(summary = "주말 외출 예약 및 업데이트 API")
     @PostMapping
@@ -33,19 +31,5 @@ public class PicnicReservationController {
     @GetMapping
     public PicnicReservationListResponse getWeekendPicnicReservation() {
         return picnicReservationApi.getPicnicReservationList();
-    }
-
-    @Operation(summary = "주말 외출 예약자 현황 엑셀 다운로드 API")
-    @GetMapping("/excel")
-    public void excel(HttpServletResponse response) throws IOException {
-        Workbook workbook = picnicReservationExel.createWorkHook();
-
-        String filename = "주말외출예약자.xlsx";
-
-        response.setContentType("ms-vnd/excel");
-        response.setHeader("Content-Disposition", "attachment;filename=" + new String(filename.getBytes("KSC5601"), "8859_1"));
-
-        workbook.write(response.getOutputStream());
-        workbook.close();
     }
 }
