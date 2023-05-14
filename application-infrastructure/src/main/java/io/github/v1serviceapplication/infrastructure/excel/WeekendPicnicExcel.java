1d1ab0b7f36bb554822dcf50a99a3f7e449420b0
jaemin <sonjm3518@dsm.hs.kr>
2023-05-14 22:23:55 +0900

    ♻️ :: 주말 외출 컬럼 수정

--- application-infrastructure/src/main/java/io/github/v1serviceapplication/infrastructure/excel/WeekendPicnicExcel.java
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/infrastructure/excel/WeekendPicnicExcel.java
@@ -3,11 +3,7 @@ package io.github.v1serviceapplication.infrastructure.excel;
 import io.github.v1serviceapplication.picnic.api.PicnicApi;
 import io.github.v1serviceapplication.picnic.api.dto.WeekendPicnicExcelElement;
 import lombok.RequiredArgsConstructor;
-import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.usermodel.IndexedColors;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Component;
 
@@ -21,56 +17,74 @@ public class WeekendPicnicExcel {
 
     public Workbook createWeekendOutingListWorkBook() {
         Workbook workbook = new XSSFWorkbook();
-
-        Sheet weekendOutingListSheet = workbook.createSheet("주말외출현황");
-        Sheet weekendOutingWaitListSheet = workbook.createSheet("주말외출대기현황");
+        Sheet sheet = workbook.createSheet("주말외출현황");
 
         List<WeekendPicnicExcelElement> weekendPicnicList =
                 picnicApi.weekendPicnicExcel().getWeekendPicnicList();
 
-        createWorkBook(workbook, weekendOutingListSheet, weekendPicnicList.stream().filter(picnic -> picnic.getIsAcceptance() == true).toList());
-        createWorkBook(workbook, weekendOutingWaitListSheet, weekendPicnicList.stream().filter(picnic -> picnic.getIsAcceptance() == false).toList());
+        createWorkBook(workbook, sheet, weekendPicnicList);
 
         return workbook;
     }
 
-    public static CellStyle cellStyle(Workbook workbook) {
-        CellStyle cellStyle = workbook.createCellStyle();
-        cellStyle.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
-
-        return cellStyle;
-    }
-
     public void createWorkBook(Workbook workbook, Sheet sheet, List<WeekendPicnicExcelElement> weekendPicnicList) {
-        CellStyle greyCellStyle = cellStyle(workbook);
+        CellStyle cellStyle = workbook.createCellStyle();
+        applyThinStyle(cellStyle);
+        Row row = sheet.createRow(0);
 
-        Row locationRow = sheet.createRow(0);
-
-        setHeaderRow(locationRow);
-        setBodyRow(sheet, weekendPicnicList);
+        setHeaderRow(row, cellStyle);
+        setBodyRow(sheet, cellStyle, weekendPicnicList);
     }
 
-    private void setBodyRow(Sheet sheet, List<WeekendPicnicExcelElement> weekendPicnicList) {
-        int cellNum = 1;
-        int rowNum = 2;
-
-        for (WeekendPicnicExcelElement weekendPicnicExcelElement : weekendPicnicList) {
-            Row row = sheet.createRow(rowNum);
-            row.createCell(cellNum).setCellValue(weekendPicnicExcelElement.getName());
-            row.createCell(cellNum + 1).setCellValue(weekendPicnicExcelElement.getNum());
-            row.createCell(cellNum + 2).setCellValue(String.valueOf(weekendPicnicExcelElement.getStartTime()));
-            row.createCell(cellNum + 3).setCellValue(String.valueOf(weekendPicnicExcelElement.getEndTime()));
-            row.createCell(cellNum + 4).setCellValue(weekendPicnicExcelElement.getReason());
-            row.createCell(cellNum + 5).setCellValue(weekendPicnicExcelElement.getArrangement());
-            rowNum++;
+    private void setHeaderRow(Row row, CellStyle cellStyle) {
+        String[] header = {"학번 ", "이름", "외출시간", "귀가시간", "", ""};
+        int wholeGrade = 24;
+        int oneClass = 6;
+        for (int currentClass = 1; currentClass <= wholeGrade; currentClass++) {
+            Cell cell = row.createCell(currentClass);
+            int index = currentClass % oneClass == 0 ? 3 : currentClass % oneClass - 1;
+            cell.setCellValue(header[index]);
+            cell.setCellStyle(cellStyle);
         }
     }
 
-    private void setHeaderRow(Row row) {
-        String[] cellValues = {"이름", "학번", "외출시간", "귀가시간", "외출사유", "동행인"};
-        int number = 1;
-        for (String values : cellValues) {
-            row.createCell(number++).setCellValue(values);
+    private void applyThinStyle(CellStyle cellStyle) {
+        cellStyle.setBorderTop(BorderStyle.THIN);
+        cellStyle.setBorderBottom(BorderStyle.THIN);
+        cellStyle.setBorderLeft(BorderStyle.THIN);
+        cellStyle.setBorderRight(BorderStyle.THIN);
+    }
+
+    private void setBodyRow(Sheet sheet, CellStyle cellStyle, List<WeekendPicnicExcelElement> weekendPicnicList) {
+        for (WeekendPicnicExcelElement weekendPicnicExcelElement : weekendPicnicList) {
+
+            int grade = Integer.parseInt(String.valueOf(weekendPicnicExcelElement.getNum().charAt(0)));
+            int cls = Integer.parseInt(String.valueOf(weekendPicnicExcelElement.getNum().charAt(1)));
+            int number = Integer.parseInt(weekendPicnicExcelElement.getNum().substring(2, 4));
+
+            int rowNo = 23 * grade - 22 + number; // 2
+            int columnNo = 4 * cls - 3; // 1
+
+            Row row = sheet.getRow(rowNo) == null ? sheet.createRow(rowNo) : sheet.getRow(rowNo);
+
+            Cell num = row.createCell(columnNo++);
+            num.setCellValue(weekendPicnicExcelElement.getNum());
+            num.setCellStyle(cellStyle);
+
+            Cell name = row.createCell(columnNo++);
+            name.setCellValue(weekendPicnicExcelElement.getName());
+            name.setCellStyle(cellStyle);
+
+            Cell startTime = row.createCell(columnNo);
+            startTime.setCellValue(String.valueOf(weekendPicnicExcelElement.getStartTime()));
+            startTime.setCellStyle(cellStyle);
+
+            Cell endTime = row.createCell(columnNo);
+            endTime.setCellValue(String.valueOf(weekendPicnicExcelElement.getEndTime()));
+            endTime.setCellStyle(cellStyle);
+
+            row.createCell(columnNo + 1).setCellStyle(cellStyle);
+            row.createCell(columnNo + 2).setCellStyle(cellStyle);
         }
     }
 }
