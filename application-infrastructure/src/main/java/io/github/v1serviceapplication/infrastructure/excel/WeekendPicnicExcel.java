a313516a61980d64a5eacf7c06529d0d51cb4cf8
jaemin <sonjm3518@dsm.hs.kr>
2023-05-15 23:39:24 +0900

    ♻️ :: 조건문 수정

--- application-infrastructure/src/main/java/io/github/v1serviceapplication/infrastructure/excel/WeekendPicnicExcel.java
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/infrastructure/excel/WeekendPicnicExcel.java
@@ -39,7 +39,7 @@ public class WeekendPicnicExcel {
     private void setHeaderRow(Row row, CellStyle cellStyle) {
         String[] header = {"학번", "이름", "외출시간", "복귀시간", "외출서명", "복귀학인"};
         cellStyle.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
-        for (int i = 1; i <= header.length; i++) {
+        for (int i = 0; i < header.length; i++) {
             Cell cell = row.createCell(i + 1);
             cell.setCellValue(header[i]);
             cell.setCellStyle(cellStyle);
@@ -54,7 +54,7 @@ public class WeekendPicnicExcel {
     }
 
     private void setBodyRow(Sheet sheet, CellStyle cellStyle, List<WeekendPicnicExcelElement> weekendPicnicList) {
-        int i = 2;
+        int i = 1;
 
         for (WeekendPicnicExcelElement weekendPicnicExcelElement : weekendPicnicList) {
             int j = 1;
