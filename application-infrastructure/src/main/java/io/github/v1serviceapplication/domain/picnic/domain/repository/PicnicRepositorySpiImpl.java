d12714f07d1e853fa7a438dc6d7270d5cd72d00c
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-05-20 23:27:36 +0900

    ♻️ :: 변수명 수정

--- application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/picnic/domain/repository/PicnicRepositorySpiImpl.java
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/picnic/domain/repository/PicnicRepositorySpiImpl.java
@@ -132,17 +132,17 @@ public class PicnicRepositorySpiImpl implements PicnicRepositorySpi {
     }
 
     private BooleanExpression checkValidLocalDateTime(DateTimePath<LocalDateTime> createDateTime, List<LocalTime> picnicTime) {
-        LocalDate yesterdayLocalDate = LocalDate.now().minusDays(1);
-        LocalDate todayLocalDate = LocalDate.now();
+        LocalDate startDate = LocalDate.now().minusDays(1);
+        LocalDate endDate = LocalDate.now();
 
         if (LocalTime.now().isAfter(picnicTime.get(0))) {
-            yesterdayLocalDate = LocalDate.now();
-            todayLocalDate = LocalDate.now().plusDays(1);
+            startDate = LocalDate.now();
+            endDate = LocalDate.now().plusDays(1);
         }
 
         return createDateTime.between(
-                LocalDateTime.of(yesterdayLocalDate, picnicTime.get(0)),
-                LocalDateTime.of(todayLocalDate, picnicTime.get(1))
+                LocalDateTime.of(startDate, picnicTime.get(0)),
+                LocalDateTime.of(endDate, picnicTime.get(1))
         );
     }
 }
