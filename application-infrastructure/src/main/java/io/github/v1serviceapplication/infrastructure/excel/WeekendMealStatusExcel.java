547200128b76587c868153e4454c29c88883a4d6
min56 <minchaeg85@gmail.com>
2023-05-29 14:05:39 +0900

    ♻️ :: 매개변수 추가

--- application-infrastructure/src/main/java/io/github/v1serviceapplication/infrastructure/excel/WeekendMealStatusExcel.java
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/infrastructure/excel/WeekendMealStatusExcel.java
@@ -26,7 +26,7 @@ public class WeekendMealStatusExcel {
 
         setHeaderRow(row1);
 
-        List<WeekendMealElement> weekendMealUserList = weekendMealApi.queryWeekendMealUserList().getStudents();
+        List<WeekendMealElement> weekendMealUserList = weekendMealApi.queryWeekendMealUserList(null, null).getStudents();
         int i = 2;
         int j = 1;
         for (WeekendMealElement weekendMealElement : weekendMealUserList) {
