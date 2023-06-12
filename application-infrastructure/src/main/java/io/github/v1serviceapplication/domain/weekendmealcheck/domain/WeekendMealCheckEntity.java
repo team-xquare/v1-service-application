3f6c1f4fb00ad2e015cce4b60b2ac276bab43508
min56 <minchaeg85@gmail.com>
2023-06-12 23:42:07 +0900

    📑 :: WeekendMealCheckEntity 추가

---
+++ application-infrastructure/src/main/java/io/github/v1serviceapplication/domain/weekendmealcheck/domain/WeekendMealCheckEntity.java
@@ -0,0 +1,43 @@
+package io.github.v1serviceapplication.domain.weekendmealcheck.domain;
+
+import io.github.v1serviceapplication.domain.weekendmeal.domain.WeekendMealEntity;
+import io.github.v1serviceapplication.global.entity.BaseDateEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@SuperBuilder
+@Table(name = "tbl_weekend_meal_check")
+@Entity
+public class WeekendMealCheckEntity extends BaseDateEntity{
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "weekend_meal_id")
+    private WeekendMealEntity weekendMeal;
+
+    @NotNull
+    @Column(columnDefinition = "TINYINT(1)")
+    private boolean isCheck;
+
+    public UUID getWeekendMealId() {
+        return this.weekendMeal.getId();
+    }
+
+    public void changeIsCheck(boolean isCheck) {
+        this.isCheck = isCheck;
+    }
+}
