eefa9b1c067da5d55bd2247650a2cbd5c536e521
min56 <minchaeg85@gmail.com>
2023-06-12 23:37:51 +0900

    📑 :: WeekendMealCheck 추가

---
+++ application-domain/src/main/java/io/github/v1serviceapplication/weekendmeakcheck/WeekendMealCheck.java
@@ -0,0 +1,19 @@
+package io.github.v1serviceapplication.weekendmeakcheck;
+
+import io.github.v1serviceapplication.annotation.Aggregate;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Aggregate
+@Getter
+@Builder
+public class WeekendMealCheck {
+    private final UUID id;
+    private final UUID userId;
+    private final LocalDate createDate;
+    private final UUID weekendMealId;
+    private final boolean isCheck;
+}
