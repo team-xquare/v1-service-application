fbeb9b2d040c2f9576452fb7a6df149d25b85694
min56 <minchaeg85@gmail.com>
2023-06-25 01:18:48 +0900

    ♻️ :: isCheck제거

--- application-domain/src/main/java/io/github/v1serviceapplication/weekendmeal/api/dto/WeekendMealListResponse.java
+++ application-domain/src/main/java/io/github/v1serviceapplication/weekendmeal/api/dto/WeekendMealListResponse.java
@@ -8,7 +8,6 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 public class WeekendMealListResponse {
-    private final Boolean isCheck;
     private final List<WeekendMealElement> responseStudents;
     private final List<WeekendMealElement> nonResponseStudents;
 }
