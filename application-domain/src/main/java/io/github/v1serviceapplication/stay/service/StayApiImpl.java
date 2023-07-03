26d75a50a3c9eaca9ae81ba8eb75f6967673253e
min56 <minchaeg85@gmail.com>
2023-07-03 23:21:31 +0900

    ♻️ :: 신청 시간 확인 메소드 추가

--- application-domain/src/main/java/io/github/v1serviceapplication/stay/service/StayApiImpl.java
+++ application-domain/src/main/java/io/github/v1serviceapplication/stay/service/StayApiImpl.java
@@ -3,6 +3,7 @@ package io.github.v1serviceapplication.stay.service;
 import io.github.v1serviceapplication.annotation.DomainService;
 import io.github.v1serviceapplication.code.CodeElement;
 import io.github.v1serviceapplication.error.UserNotFoundException;
+import io.github.v1serviceapplication.stay.exception.CanNotStayApplyException;
 import io.github.v1serviceapplication.user.UserIdFacade;
 import io.github.v1serviceapplication.stay.Stay;
 import io.github.v1serviceapplication.stay.api.StayApi;
@@ -20,11 +21,17 @@ import io.github.v1serviceapplication.stay.exception.AlreadyExistsStayException;
 import io.github.v1serviceapplication.stay.spi.PointUserFeignSpi;
 import io.github.v1serviceapplication.stay.spi.StayRepositorySpi;
 import io.github.v1serviceapplication.stay.spi.StayUserFeignSpi;
+import io.github.v1serviceapplication.weekendmeal.WeekendMeal;
 import io.github.v1serviceapplication.weekendmeal.WeekendMealApplicationStatus;
 import lombok.RequiredArgsConstructor;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -73,8 +80,19 @@ public class StayApiImpl implements StayApi {
 
     @Override
     public void applyStay(StayStatusCode status) {
+        validCheckApplyStayTime();
         UUID userId = userIdFacade.getCurrentUserId();
         stayRepositorySpi.applyStay(userId, status);
+    }
+
+    private void validCheckApplyStayTime() {
+        boolean validRequestWeek = !LocalDate.now().getDayOfWeek().equals(DayOfWeek.THURSDAY);
+        boolean validRequestStartTime  = LocalTime.now().isAfter(LocalTime.parse("23:00:00.00"));
+        boolean validRequestEndTime = LocalTime.now().isBefore(LocalTime.parse("20:30:00"));
+
+        if(validRequestWeek || validRequestStartTime || validRequestEndTime) {
+            throw CanNotStayApplyException.EXCEPTION;
+        }
     }
 
     @Override
